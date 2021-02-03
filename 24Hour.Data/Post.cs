b4b63ec94750f07f7fb33e7421fa741df930c857
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-02-03 00:37:56 -0500

    scaffold of post classes

---
+++ 24Hour.Data/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        //virtual list of comments
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+
+
+        //Guid Author(using user ID)??
+        public Guid Author { get; set; }
+        
+        //should probably add some range, max length and error message to properties
+    }
+}
---
+++ 24Hour.Services/PostService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+    }
+}
