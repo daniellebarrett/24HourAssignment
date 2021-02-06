6f8bfbe0fefcfb8b63a062ba2159c015263cda4b
Sammourfield <sammourfie@gmail.com>
2021-02-06 06:53:12 -0800

    here we go

--- 24Hour.Data/IdentityModels.cs
+++ 24Hour.Data/IdentityModels.cs
@@ -51,7 +51,7 @@ namespace _24Hour.Data
         }
 
         public DbSet<Post> Posts { get; set; }
-    //    public DbSet<Comment> Comments { get; set; }
+        public DbSet<Comment> Comments { get; set; }
 
       //  public DbSet<Reply> Replies { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
