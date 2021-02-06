f7c57b675f5e29fe4fa3050ef50a023130353557
daniellebarrett <daniellebarrett@alumni.iu.edu>
2021-02-06 11:43:24 -0500

    finished postController

--- 24HourAssignment/Controllers/PostController.cs
+++ 24HourAssignment/Controllers/PostController.cs
@@ -14,21 +14,6 @@ namespace _24HourAssignment.Controllers
     [Authorize]
     public class PostController : ApiController
     {
-        private PostService CreatePostService()
-        {
-            var postId = Guid.Parse(User.Identity.GetUserId());
-            var postService = new PostService(postId);
-            return postService;
-        }
-
-
-        public IHttpActionResult Get()
-        {
-            PostService postService = CreatePostService();
-            var posts = postService.GetPosts();
-            return Ok(posts);
-        }
-
         public IHttpActionResult Post(PostCreate post)
         {
             if (!ModelState.IsValid)
@@ -41,8 +26,43 @@ namespace _24HourAssignment.Controllers
 
             return Ok();
         }
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
 
+        public IHttpActionResult Get(int id)
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPostById(id);
+            return Ok(post);
+        }
 
+        public IHttpActionResult Put (PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreatePostService();
+            if (!service.UpdatePost(post))
+                return InternalServerError();
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+            if (!service.DeletePost(id))
+                return InternalServerError();
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var postId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(postId);
+            return postService;
+        }
 
     }
 }
